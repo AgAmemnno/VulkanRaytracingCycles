1fa55214487755d3aca16cea942878c168533852
AgAmemnno <kaz380@hotmail.co.jp>
2022-08-30 10:31:41 +0900

    Update on 2022-08-30

--- docs/navtreedata.js
+++ docs/navtreedata.js
@@ -24,58 +24,12 @@
 */
 var NAVTREE =
 [
-  [ "VulkanRaytracingCycles", "index.html", [
-    [ "Namespaces", "namespaces.html", [
-      [ "Namespace List", "namespaces.html", "namespaces_dup" ],
-      [ "Namespace Members", "namespacemembers.html", [
-        [ "All", "namespacemembers.html", null ],
-        [ "Functions", "namespacemembers_func.html", null ],
-        [ "Variables", "namespacemembers_vars.html", null ],
-        [ "Typedefs", "namespacemembers_type.html", null ],
-        [ "Enumerations", "namespacemembers_enum.html", null ],
-        [ "Enumerator", "namespacemembers_eval.html", null ]
-      ] ]
-    ] ],
-    [ "Classes", "annotated.html", [
-      [ "Class List", "annotated.html", "annotated_dup" ],
-      [ "Class Index", "classes.html", null ],
-      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
-      [ "Class Members", "functions.html", [
-        [ "All", "functions.html", "functions_dup" ],
-        [ "Functions", "functions_func.html", "functions_func" ],
-        [ "Variables", "functions_vars.html", "functions_vars" ],
-        [ "Typedefs", "functions_type.html", null ],
-        [ "Enumerations", "functions_enum.html", null ],
-        [ "Enumerator", "functions_eval.html", null ]
-      ] ]
-    ] ],
-    [ "Files", "files.html", [
-      [ "File List", "files.html", "files_dup" ],
-      [ "File Members", "globals.html", [
-        [ "All", "globals.html", "globals_dup" ],
-        [ "Functions", "globals_func.html", null ],
-        [ "Variables", "globals_vars.html", null ],
-        [ "Typedefs", "globals_type.html", null ],
-        [ "Enumerations", "globals_enum.html", null ],
-        [ "Enumerator", "globals_eval.html", null ],
-        [ "Macros", "globals_defs.html", null ]
-      ] ]
-    ] ]
-  ] ]
+  [ "VulkanRaytracingCycles", "index.html", ]
 ];
 
 var NAVTREEINDEX =
 [
-"_allocator_vk_8cpp.html",
-"_uniforms_vk_8h.html#a4bf726b236af2a7f9d6fd75958544b97",
-"functions_o.html",
-"namespace_harness_1_1internal.html#a97f2c6a3a36e1362139032340edec92a",
-"struct_debug_master.html#ac51f7c1661a8f40ab04a5db69b95127b",
-"struct_m_bvk.html#a4240e2f0c0d55cfe11466bb198b04083",
-"struct_tex_bache.html#ae20d079f0ac1d561f81b111b9ce54e98",
-"structfront_1_1heap_q.html#a5a83d33d7f91968b4a0431929be4999b",
-"structvkmm_1_1_allocation___t.html#ab969726346157413ef6f2336acf70843",
-"structvkmm_1_1_pointer_less.html#a4ed5e6e3376e7cf3bcfbcc5b7e9f2fab"
+"index.html"
 ];
 
 var SYNCONMSG = 'click to disable panel synchronisation';
